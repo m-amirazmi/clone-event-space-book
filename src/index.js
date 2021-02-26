a540494cd8d7890273159d27eb6b090163c1d8c5
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2021-02-27 02:07:40 +0800

    start the first clone project - event book system

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import { Home } from './pages/Home';
+
+ReactDOM.render(<StrictMode><Home/></StrictMode>, document.getElementById('root'));
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@material-ui/core';
+
+export const Home = () => {
+    return (
+        <div>
+            This is the home page
+             <Button>Test Button</Button>
+        </div>
+    )
+}
+
